b94c97789917251f8f9a401a10768e70d9eda7c1
TowerOne <TowerOne@TOWERONE-PC>
2017-01-27 14:59:05 -0600

    added fabric.js

---
+++ Frame.js
@@ -0,0 +1,10 @@
+﻿var canvas = document.getElementById("myCanvas");
+var rec = new fabric.Rect(
+    {
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 20,
+        height: 20
+    });
+canvas.add(rec);
