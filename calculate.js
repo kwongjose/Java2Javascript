213fc45f916e25fd916157d09f713c89eee4444d
TowerOne <TowerOne@TOWERONE-PC>
2017-02-05 01:26:33 -0600

    add calculate

---
+++ calculate.js
@@ -0,0 +1,48 @@
+columns: [
+    {
+        top: [false, false],
+        bottom: [false, false, false, false, false]
+    },
+    {
+        top: [false, false],
+        bottom: [false, false, false, false, false]
+    },
+    {
+        top: [false, false],
+        bottom: [false, false, false, false, false]
+    },
+    {
+        top: [false, false],
+        bottom: [false, false, false, false, false]
+    },
+    {
+        top: [false, false],
+        bottom: [false, false, false, false, false]
+    },
+    {
+        top: [false, false],
+        bottom: [false, false, false, false, false]
+    },
+    {
+        top: [false, false],
+        bottom: [false, false, false, false, false]
+    },
+    {
+        top: [false, false],
+        bottom: [false, false, false, false, false]
+    }
+]
+
+/*
+/takes a input of the coloumn level and place of a bead
+/ sets the bool of the bead to its inverse
+**/
+var flipBead = function (col, lev, place) {
+    if (lev) {//true if top
+        columns[c].top[place] = !columns[c].top[place];
+    }
+    else {
+        columns[c].bottom[place] = !columns[c].bottom[place];
+    }
+    
+};
