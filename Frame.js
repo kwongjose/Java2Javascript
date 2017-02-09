6d84c33c7fcae23b5ea6d2420651c5dad2849bef
TowerOne <TowerOne@TOWERONE-PC>
2017-02-08 22:01:57 -0600

    spelling

--- Frame.js
+++ Frame.js
@@ -3,27 +3,27 @@ var TBrec;
 var canvas;
 var sel;
 var load = function () {
-canvas = new fabric.Canvas("myCanvas");
+    canvas = new fabric.Canvas("myCanvas");
 
-var x = 110;
-for (var i = 0; i < 8; i++) {
-    var nLines = makeLines(x);
-    nLines.selectable = false;
-    x += 110;
-    canvas.add(nLines);
-}
+    var x = 110;
+    for (var i = 0; i < 8; i++) {
+        var nLines = makeLines(x);
+        nLines.selectable = false;
+        x += 110;
+        canvas.add(nLines);
+    }
     //bottom box
-Brec = new fabric.Rect({ width: 989, height: 280, left: 6, top: 172, selectable: false, fill: 'rgba(0,0,0,0)', stroke: "black" });
-Brec.lockMovementX = true; Brec.lockMovementY = true; 
-canvas.add(Brec);
-TBrec = new fabric.Rect({ width: 989, height: 138, left: 6, top: 32, selectable: false, fill: 'rgba(0,0,0,0', stroke: "black" });
-TBrec.lockMovementX = true; TBrec.lockMovementY = true; 
-canvas.add(TBrec);
+    Brec = new fabric.Rect({ width: 989, height: 280, left: 6, top: 172, selectable: false, fill: 'rgba(0,0,0,0)', stroke: "black" });
+    Brec.lockMovementX = true; Brec.lockMovementY = true;
+    canvas.add(Brec);
+    TBrec = new fabric.Rect({ width: 989, height: 138, left: 6, top: 32, selectable: false, fill: 'rgba(0,0,0,0', stroke: "black" });
+    TBrec.lockMovementX = true; TBrec.lockMovementY = true;
+    canvas.add(TBrec);
 
-makeBeads(canvas);
-//canvas.on("mouse:over", moveHandler);
-canvas.on("object:moving", checkBounds);
-canvas.on("object:selected", Starting);
+    makeBeads(canvas);
+    //canvas.on("mouse:over", moveHandler);
+    canvas.on("object:moving", checkBounds);
+    canvas.on("object:selected", Starting);
 }
 
 var Starting = function (evt) {
@@ -53,7 +53,7 @@ var checkBounds = function (evt) {
             var leftBound = Brec.left;
             var rightBound = leftBound + Brec.width;
         }
-       // obj_type.setLeft(Math.min(Math.max(left, leftBound), rightBound - evt.width));
+        // obj_type.setLeft(Math.min(Math.max(left, leftBound), rightBound - evt.width));
         obj_type.setTop(Math.min(Math.max(top, topBound), bottomBound - obj_type.height));
         intersectingCheck(obj_type);
     }//end if
@@ -61,9 +61,9 @@ var checkBounds = function (evt) {
 
 var moveHandler = function (evt) {
     var moving = evt.target;
-     console.log(moving.top + "left: " + moving.left + "W: " + moving.type );
+    console.log(moving.top + "left: " + moving.left + "W: " + moving.type);
     //console.log(moving.getOriginY() + "X: " + moving.getOriginX() + "SX: " + moving.getScaleX() + "SY: " + moving.getScaleY());
-    
+
 }
 
 //makes lines for abacus
@@ -75,30 +75,32 @@ var makeBeads = function (canvas) {
     var left_Scal = 89;
     var vertic_Scal = 411;//for bottom
     var vert_Scal = 33; //for top
-    for(var i = 0; i < 8; i++){
+    for (var i = 0; i < 8; i++) {
+        var tb = 0;
         for (var t = 0; t < 7; t++) {
             if (t < 5) {//make bottom beads
                 var beads = new fabric.Circle({ top: vertic_Scal, left: left_Scal, radius: 20, fill: "red" });
-                beads.collomn = i;
+                beads.columns = i;
                 beads.level = false;
                 beads.lockMovementX = true;
                 beads.place = t;
                 canvas.add(beads);
-                vertic_Scal -=41;
+                vertic_Scal -= 41;
             }
             else {//make top beads
                 var beads = new fabric.Circle({ top: vert_Scal, left: left_Scal, radius: 20, fill: "red" });
-                beads.collomn = i;
+                beads.columns = i;
                 beads.level = true;
                 beads.lockMovementX = true;
-                beads.place = t;
+                beads.place = tb;
                 canvas.add(beads);
                 vert_Scal += 41;
+                tb++;
             }
         }
         left_Scal += 110;//move to next col
-        vertic_Scal = 411;
-        vert_Scal = 33;
+        vertic_Scal = 411;//location of top first bead on the bottom
+        vert_Scal = 33;//location of the first bead on top
     }
 }
 
